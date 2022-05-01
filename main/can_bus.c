c6f9a3fc2e43c147cf5fe6b4c297795082aeac63
Demetry <demetryromanowski@gmail.com>
2022-05-01 00:12:29 -0400

    Finalized CRC on message queue, and defined message structure

--- main/can_bus.c
+++ main/can_bus.c
@@ -3,6 +3,10 @@
 
 #include <string.h>
 
+long long microsecond_time; 
+
+comms_message_t generate_message(long long time, can_message_type message_type, uint32_t id, void* data, size_t data_len); 
+
 esp_err_t can_bus_init(can_config_t settings) { 
     esp_err_t last_err = ESP_OK; 
     last_err = twai_driver_install(&settings.g_config, &settings.t_config, &settings.f_config);
@@ -12,8 +16,12 @@ esp_err_t can_bus_init(can_config_t settings) {
     ESP_LOGI(TAG, "Driver started");
     // send_data(CAN_DRIVER_STARTED);
 
+    microsecond_time = 0; 
+
     return last_err; 
 }
+
+long long last_time = 0; 
 
 int can_bus_update(bool update) {
     if(!update)
@@ -27,18 +35,9 @@ int can_bus_update(bool update) {
     }
 
     if(!message.rtr) {
-        uint8_t identifier_arr[4]; 
-        identifier_arr[0] = (message.identifier >> 24) & 0xff; 
-        identifier_arr[1] = (message.identifier >> 16) & 0xff; 
-        identifier_arr[2] = (message.identifier >> 8) & 0xff; 
-        identifier_arr[3] = (message.identifier) & 0xff; 
+        microsecond_time = esp_timer_get_time(); 
 
-        uint8_t complete_arr[message.data_length_code + 4]; 
-
-        memcpy(complete_arr, identifier_arr, 4); 
-        memcpy(complete_arr + 4, message.data, message.data_length_code); 
-
-        create_message(&com_message, complete_arr, message.data_length_code + 4); 
+        com_message = generate_message(microsecond_time - last_time, STANDARD_FRAME, message.identifier, message.data, message.data_length_code);
         add_message(com_message);
 
 #ifdef CAN_DEBUG
@@ -47,9 +46,15 @@ int can_bus_update(bool update) {
         }
         printf("\n"); 
 #endif
-
+        last_time = microsecond_time; 
     } else { 
         //TODO(Demi): Send remote frame status
+        microsecond_time = esp_timer_get_time(); 
+
+        com_message = generate_message(microsecond_time - last_time, REMOTE_FRAME, message.identifier, NULL, 0);
+        add_message(com_message);
+
+        last_time = microsecond_time; 
     }    
 
     return 1; 
@@ -68,4 +73,50 @@ esp_err_t can_bus_cleanup() {
     ESP_LOGI(TAG, "Driver uninstalled");
     // send_data(CAN_DRIVER_UNINSTALLED);
     return last_err; 
+}
+
+/**
+ * @brief Generate a message from can data
+ * 
+ * @param time 
+ * @param id 
+ * @param data 
+ * @param data_len 
+ * @return comms_message_t 
+ */
+comms_message_t generate_message(long long time, can_message_type message_type, uint32_t id, void* data, size_t data_len) { 
+    comms_message_t message; 
+
+    uint16_t type = (uint16_t)message_type;
+
+    uint8_t type_arr[2]; 
+    type_arr[0] = (type >> 8) & 0xff; 
+    type_arr[1] = (type) & 0xff; 
+
+    uint8_t id_arr[4]; 
+    id_arr[0] = (id >> 24) & 0xff; 
+    id_arr[1] = (id >> 16) & 0xff; 
+    id_arr[2] = (id >> 8) & 0xff; 
+    id_arr[3] = (id) & 0xff; 
+
+    uint8_t time_arr[8]; 
+    time_arr[0] = (time >> 56) & 0xff; 
+    time_arr[1] = (time >> 48) & 0xff; 
+    time_arr[2] = (time >> 40) & 0xff; 
+    time_arr[3] = (time >> 32) & 0xff; 
+    time_arr[4] = (time >> 24) & 0xff; 
+    time_arr[5] = (time >> 16) & 0xff; 
+    time_arr[6] = (time >> 8) & 0xff; 
+    time_arr[7] = (time) & 0xff;
+
+    uint8_t data_arr[data_len + 14]; 
+
+    memcpy(data_arr, time_arr, 8);
+    memcpy(data_arr + 8, type_arr, 2);  
+    memcpy(data_arr + 10, id_arr, 4); 
+    memcpy(data_arr + 14, data, data_len); 
+
+    create_message(&message, data_arr, data_len + 14); 
+
+    return message; 
 }
--- main/can_bus.h
+++ main/can_bus.h
@@ -18,6 +18,11 @@
 
 #define TAG "CAN BUS"
 
+typedef enum can_message_type { 
+    STANDARD_FRAME, 
+    REMOTE_FRAME
+} can_message_type; 
+
 typedef struct can_config_t { 
     twai_general_config_t g_config; 
     twai_timing_config_t t_config; 
